04c31d9bc8be181d2f4453742eede025b6b34dc9
d.takeuchi <daiki.takeuchi.0327@gmail.com>
2016-12-05 09:23:06 +0900

    product_line画面が表示されるように修正

--- application/controllers/product_line.py
+++ application/controllers/product_line.py
@@ -1,16 +1,18 @@
 from flask import Blueprint
 from flask import render_template
 
+from application.domain.product_line import ProductLine
+
 app = Blueprint('product_line', __name__)
 
 
 @app.route('/')
 def index():
-    result = []
+    result = ProductLine.query.all()
     return render_template('product_line/index.html', result=result)
 
 
 @app.route('/detail/<product_line_id>')
 def detail(product_line_id):
-    product_line = []
-    return render_template('product_line/detail.html', result=product_line)
+    product_line = ProductLine.query.filter('product_line.id = ' + product_line_id).one()
+    return render_template('product_line/detail.html', product_line=product_line)
---
+++ application/domain/product_line.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Numeric
+
+from application.config.database import Base
+
+
+class ProductLine(Base):
+    __tablename__ = 'product_line'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    product_line = Column(String(32), nullable=False)
+    text_description = Column(String(1024), nullable=False)
+    html_description = Column(String(1024), nullable=False)
+    image = Column(String(1024), nullable=False)
+
+    def __init__(self,
+                 product_line=None,
+                 text_description=None,
+                 html_description=None,
+                 image=None):
+        self.product_line = product_line
+        self.text_description = text_description
+        self.html_description = html_description
+        self.image = image
+
+    def __repr__(self):
+        return '<ProductLine {}[ID:{}>'.format(self.product_line, self.id)
